e1598e632c65232ca2e9aa352362a6b1bc5e4fb9
abhinavaSurabhi <abhinava.surabhi@gmail.com>
2021-12-07 18:08:53 +0530

    Changes to not use Math module in BrickStaircase problem

--- BrickStaircase.js
+++ BrickStaircase.js
@@ -1,7 +1,15 @@
 function brickStairCase() {
     var bricks = parseInt(prompt("Please enter the number", "0"));
-    var root = (-1.0 + Math.sqrt(1 + 8 * bricks)) / 2;
+    //var root = (-1 + Math.sqrt(1 + (8 * bricks)))/2;
+    var sum = 0;
+    var n = 0;
+    while (sum <= bricks) {
+        n++;
+        sum += n;
+    }
+    n--;
+    console.log(n);
     document.getElementById("brickStaircaseOutput").innerHTML =
-        Math.floor(root) + " stairs can be constructed";
-    console.log(Math.floor(root));
+        n + " stairs can be constructed";
+    //console.log(Math.floor(n));
 }
